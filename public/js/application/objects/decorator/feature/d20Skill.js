4eca1cd7f060ebef4683270efb5198062bceb9f8
Rachid <rachid@actived.fr>
2014-03-05 16:41:12 +0100

    Skill with skill points

--- public/js/application/objects/decorator/feature/d20Skill.js
+++ public/js/application/objects/decorator/feature/d20Skill.js
@@ -1,5 +1,5 @@
-var Decorator_Feature_D20Ability = Decorator_Feature.extend({
-    bonus: function(){
-
+var Decorator_Feature_D20Skill = Decorator_Feature.extend({
+    getValue: function(){
+        return Number(this.sheet.getFeatureFromCode(this.config.basedOnFeature).score()) + this.sheet.config.features[this.id].skillPoints;
     }
 });
